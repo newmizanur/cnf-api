93e4585546fd6ea9badb57a5211596199ec7002a
newmizanur <newmizanur@gmail.com>
2018-09-11 15:31:19 +0600

    remove: removed open auth related things

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,13 +1,13 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const BearerStrategy = require('passport-http-bearer');
-const { ExtractJwt } = require('passport-jwt');
-const { jwtSecret } = require('./vars');
-const authProviders = require('../api/services/authProviders');
-const User = require('../api/models/user.model');
+const JwtStrategy = require("passport-jwt").Strategy;
+const BearerStrategy = require("passport-http-bearer");
+const { ExtractJwt } = require("passport-jwt");
+const { jwtSecret } = require("./vars");
+// const authProviders = require("../api/services/authProviders");
+const User = require("../api/models/user.model");
 
 const jwtOptions = {
   secretOrKey: jwtSecret,
-  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("Bearer")
 };
 
 const jwt = async (payload, done) => {
@@ -20,16 +20,4 @@ const jwt = async (payload, done) => {
   }
 };
 
-const oAuth = service => async (token, done) => {
-  try {
-    const userData = await authProviders[service](token);
-    const user = await User.oAuthLogin(userData);
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-};
-
 exports.jwt = new JwtStrategy(jwtOptions, jwt);
-exports.facebook = new BearerStrategy(oAuth('facebook'));
-exports.google = new BearerStrategy(oAuth('google'));
